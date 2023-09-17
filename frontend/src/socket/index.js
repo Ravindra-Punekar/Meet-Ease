c88295d7a030e45e002567fa63f823bcf94ba711
Ravindra Punekar <punekarravi20@gmail.com>
2023-09-17 19:33:57 +0530

    Fix: WebSocket connection not handling disconnects properly

--- frontend/src/socket/index.js
+++ frontend/src/socket/index.js
@@ -1,12 +1,45 @@
-import {io} from 'socket.io-client';
+// import {io} from 'socket.io-client';
+
+// export const socketInit = () => {
+//     const options = {
+//         'force new connection': true,
+//         reconnectionAttempts: 'Infinity',
+//         timeout: 10000,
+//         transports: ['websocket'],
+//     };
+
+//     return io('http://localhost:5500', options);
+// }
+
+import { io } from 'socket.io-client';
 
 export const socketInit = () => {
-    const options = {
-        'force new connection': true,
-        reconnectionAttempts: 'Infinity',
-        timeout: 10000,
-        transports: ['websocket'],
-    };
+  const options = {
+    'force new connection': true,
+    reconnectionAttempts: 'Infinity',
+    timeout: 10000,
+    transports: ['websocket'],
+    reconnectionDelay: 5000, // Set the reconnection delay to 5 seconds (5000 milliseconds)
+  };
 
-    return io('http://localhost:5500', options);
-}
+  const socket = io('http://localhost:5500', options);
+
+  let reconnectTimer; // Define a timer variable
+
+  socket.on('connect', () => {
+    console.log('WebSocket connection established');
+    // Clear the timer if the connection is re-established
+    clearTimeout(reconnectTimer);
+  });
+
+  socket.on('disconnect', () => {
+    console.error('WebSocket connection closed due to disconnect');
+
+    // Set a timer to redirect to the home page after 5 seconds
+    reconnectTimer = setTimeout(() => {
+      window.location.href = '/'; // Redirect to the home page
+    }, 5000); // 5000 milliseconds (5 seconds) delay
+  });
+
+  return socket;
+};
